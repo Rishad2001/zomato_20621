688d6033e090f3b55f6299d9023e78dd96001428
unknown <r23richu@gmail.com>
2021-09-23 13:47:46 +0530

    day 53and 54

---
+++ server/database/restaurant/index.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const RestaurantSchema = new mongoose.Schema({
+    name: { type: String, requierd: true },
+    city: { type: String, requierd: true },
+    address: { type: String, requierd: true },
+    mapLocation: { type: String, requierd: true },
+    cuisine: [String],
+    restaurantTimings: String,
+    contactNumber: Number,
+    website: String,
+    popularDishes: [String],
+    averageCost: Number,
+    amenities: [String],  //amenities-> featuers
+    menuImages: {
+        type: mongoose.Types.ObjectId, ref: "Images"
+    },
+    menu: {
+        type: mongoose.Types.ObjectId, ref: "Images"
+    },
+    reviews: [{
+        type: mongoose.Types.ObjectId, ref: "Reviews"
+    }],
+    photos: {
+        type: mongoose.Types.ObjectId, ref: "Images"
+    },
+},
+{
+    timesamps: true
+});
+
+export const RestaurantModel = mongoose.model("Restaurants", RestaurantSchema);
